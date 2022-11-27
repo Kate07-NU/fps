329d17bca04c3de7b7914f5b69b79465c116c2c5
Kate07-NU <kaitoono2003@gmail.com>
2022-11-28 01:29:55 +0900

    2

--- Assets/AllAssets/Scripts/ReturnMainCamera.cs
+++ Assets/AllAssets/Scripts/ReturnMainCamera.cs
@@ -6,6 +6,7 @@ public class ReturnMainCamera : MonoBehaviour
 {
     [SerializeField] Camera subTVCamera = default;
     [SerializeField] Camera subChestCamera = default;
+    [SerializeField] Camera subPanelCamera = default;
     [SerializeField] Camera mainCamera = default;
     [SerializeField] GameObject backPanel = default;
 
@@ -18,6 +19,10 @@ public class ReturnMainCamera : MonoBehaviour
             backPanel.SetActive(false);
         } else if (subChestCamera.gameObject.activeSelf == true) {
             subChestCamera.gameObject.SetActive(false);
+            mainCamera.gameObject.SetActive(true);
+            backPanel.SetActive(false);
+        } else if (subPanelCamera.gameObject.activeSelf == true) {
+            subPanelCamera.gameObject.SetActive(false);
             mainCamera.gameObject.SetActive(true);
             backPanel.SetActive(false);
         }
