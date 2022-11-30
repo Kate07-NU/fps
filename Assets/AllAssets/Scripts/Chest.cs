7e969d7faecad6d8eb9d0e0e556bc92184290bcf
Kate07-NU <kaitoono2003@gmail.com>
2022-11-30 11:36:30 +0900

    5

--- Assets/AllAssets/Scripts/Chest.cs
+++ Assets/AllAssets/Scripts/Chest.cs
@@ -8,10 +8,16 @@ public class Chest : MonoBehaviour
 
     [SerializeField] Animation anim = default;
 
+    void Start()
+    {
+        // 最初は宝のアイテムを非表示にする
+    }
+
     public void Open()
     {
         if (ItemBox.instance.CheckSelectItem(Item.Type.Key)) {
             anim.Play();
+            // ここで宝のアイテムを表示する
             ItemBox.instance.UseSelectItem();
         }
     }
